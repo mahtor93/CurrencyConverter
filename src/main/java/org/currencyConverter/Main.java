f00c761066516684d9132fcc51169b4d08730231
Mario H. Tornería Ramírez <mario.torneria@virginiogomez.cl>
2025-11-24 16:51:21 -0300

    Creación de función para conectar a la URL del sistema conversor. Prueba de archivo de variables protegidas que no subiré a github a menos que el challenge lo requiera

--- src/main/java/org/currencyConverter/Main.java
+++ src/main/java/org/currencyConverter/Main.java
@@ -1,9 +1,15 @@
 package org.currencyConverter;
 
+import org.currencyConverter.connections.Connection;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        System.out.println(
+                Connection.request("fetch-one?from=USD&to=EUR")
+        );
     }
 }
--- src/main/java/org/currencyConverter/connections/Connection.java
+++ src/main/java/org/currencyConverter/connections/Connection.java
@@ -1,21 +1,40 @@
 package org.currencyConverter.connections;
 
+import org.currencyConverter.Main;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Properties;
 
 public class Connection {
-    protected static HttpResponse<String> request(String uri, String query) throws IOException {
+
+
+
+    public static HttpResponse<String> request(String query) throws IOException {
         try {
+
+            Properties props = new Properties();
+            try (InputStream is = Main.class.getResourceAsStream("/config.properties")) {
+                props.load(is);
+            } catch (Exception ex){
+                System.out.println(ex);
+            }
+
+            String API_KEY = props.getProperty("API_KEY");
+
             HttpClient client = HttpClient.newBuilder()
                     .build();
 
             HttpRequest req = HttpRequest.newBuilder()
                     .uri(URI.create(
-                            String.format("%s/%s", uri, query)
+                            String.format("https://api.fastforex.io/%s",query)
                     ))
+                    .header("Authorization","Bearer "+API_KEY)
+                    .GET()
                     .build();
 
             return client.send(req, HttpResponse.BodyHandlers.ofString());
